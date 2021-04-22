4c5d33b4e2c544089751c2c4e2f01cbf85820231
rafaelfelip <orafaelfelip@gmail.com>
2021-04-21 22:08:43 -0300

    dia 02

---
+++ src/pages/UserIndentification.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet
+ } from 'react-native';
+
+export function UserIndentification(){
+  return(
+    <SafeAreaView style={styles.container}>
+
+    </SafeAreaView>
+
+  )
+}
+
+
+const styles = StyleSheet.create({
+  container:
+});
--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -5,70 +5,81 @@ import {
   Image, 
   TouchableOpacity, 
   StyleSheet,
-  Dimensions 
+  Dimensions,
+  View 
 } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 
 import wateringImg from '../assets/watering.png';
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
+
 
 export function Welcome() {
   return ( 
-    <SafeAreaView 
-    style={styles.container}>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.wrapper}>
+
       <Text 
       style={styles.title}>
       {/* para dar quebra de linhas */}
         Gerencie {'\n'}  
-        suas plantas {'\n'}
-        de forma fácil
+        suas plantas de {'\n'}
+        forma fácil
       </Text>
    
       <Image 
-       source={wateringImg}
-       style={styles.image} 
-       resizeMode='contain'
+        source={wateringImg}
+        style={styles.image} 
+        resizeMode='contain'
       />
   
       <Text style={styles.subTitle}>
-          Não esqeça mais de regar suas plantas. 
-          Nós cuidamos de lembrar você sempre que precisar.
+        Não esqeça mais de regar suas plantas. Nós {'\n'} 
+        cuidamos de lembrar você sempre que {'\n'} 
+        precisar.
       </Text>
 
       <TouchableOpacity 
-    style={styles.button} 
-    activeOpacity={0.6}>
-
-        <Text>
-         <Feather 
-         name="chevron-right"
-         style={styles.buttonIcon} />
-        </Text>
-
+        style={styles.button} 
+        activeOpacity={0.6}>
+      
+      <Feather 
+        name="chevron-right"
+        style={styles.buttonIcon} />
+        
       </TouchableOpacity>  
+
+      </View>
     </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
+    flex: 1,    
+  },
+  wrapper: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'space-around',
-    
+    paddingHorizontal: 20,
   },
   title: {
-    marginTop: 50,
-    fontSize: 32,
+    fontSize: 28,
     fontWeight: 'bold',
     textAlign: 'center',
     color: colors.heading,
+    marginTop: 30,
+    fontFamily: fonts.heading,
+    lineHeight: 35,
   },
   subTitle: {
     textAlign: 'center',
     fontSize: 18,
     paddingHorizontal: 20,
     color: colors.heading,
+    fontFamily: fonts.text,
   },
   image: {
     // para responsividade não cravar um tamanho correto olha na image prop risizeMode
